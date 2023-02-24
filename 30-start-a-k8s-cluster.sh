f9dddd29ac02b1fa51202ccfb9b0dede679b6c4b
Ludwig Prager <ludwig.prager@celp.de>
2023-02-24 18:34:21 +0100

    fixes clone command

--- 30-start-a-k8s-cluster.sh
+++ 30-start-a-k8s-cluster.sh
@@ -12,18 +12,4 @@ source ./set-env.sh
 if ! cluster-exists $CLUSTER; then
   ./k3d cluster create $CLUSTER \
     --wait 
-
-#     --config k3d-config/$cluster.yaml \
-
 fi
-
-# create endpoint in cluster pointing to my primary IP address
-kubectl apply -f manifest/namespace.yaml
-
-#  export PRIMARY_IP=$(get-primary-ip)
-#  envsubst < manifest/external-service.yaml.tpl | kubectl apply -nkafka -f -
-
-
-
-#echo "waiting for metrics server pod to get ready"
-#kubectl wait --for=condition=ready --timeout=90s -nkube-system pod -l k8s-app=metrics-server
--- 40-start-kafka-cluster.sh
+++ 40-start-kafka-cluster.sh
@@ -32,6 +32,7 @@ source ./set-env.sh
 
 #kubectl get pods -n kafka -w
 
+kubectl apply -f manifest/namespace.yaml
 
 kubectl create -f 'https://strimzi.io/install/latest?namespace=kafka' -n kafka
 kubectl apply -f https://strimzi.io/examples/latest/kafka/kafka-persistent-single.yaml -n kafka 
